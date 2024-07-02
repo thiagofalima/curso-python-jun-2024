f1130bcad33ef80f5a222fa88f7192a82f9326da
thiagofalima <thiagofalima@hotmail.com>
2024-07-02 19:03:37 -0300

    Códigos aula 01/07

---
+++ capitulo-6/area_circunferencia.py
@@ -0,0 +1,12 @@
+def area_circunferencia():
+
+    raio = float(input('Entre com o raio da circunferência: '))
+    pi = 3.14
+
+    return pi * raio ** 2
+
+
+resultado = area_circunferencia()
+
+print(resultado)
+
